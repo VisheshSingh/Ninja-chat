2339a9a9704e524c4ed215380c5e15aa76d320c2
Vishesh <visheshsingh16@gmail.com>
2018-09-29 00:57:47 -0400

    Firestore setup

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import firestore from "firebase/firestore";
+
+var config = {
+  apiKey: "AIzaSyBwp9fNKHKfCz4S0kFesCWVso8JCIqIeoQ",
+  authDomain: "ninja-chat-8cbdd.firebaseapp.com",
+  databaseURL: "https://ninja-chat-8cbdd.firebaseio.com",
+  projectId: "ninja-chat-8cbdd",
+  storageBucket: "",
+  messagingSenderId: "391407378260"
+};
+
+var firebaseApp = firebase.initializeApp(config);
+export default firebaseApp.firestore;
